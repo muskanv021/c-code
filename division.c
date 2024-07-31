ada306f9f897a763e981b42872aab2891dc079dd
Muskan Vaishnav <120307276+muskanv021@users.noreply.github.com>
2024-07-31 23:57:30 +0530

    Create division.c

---
+++ division.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a, b;
+    float c;
+    printf("Enter 2 number values: ");
+    scanf("%d %d", &a, &b);
+    
+    if (b != 0) {
+        c = (float)a / b;
+        printf("\nDivision of %d and %d is %.2f", a, b, c);
+    } else {
+        printf("\nCannot divide by zero!!");
+    }
+
+    return 0;
+}
