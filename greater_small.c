cd3812a244e9e348c84bb223c7837d02bb03bcbd
Muskan Vaishnav <120307276+muskanv021@users.noreply.github.com>
2024-08-01 00:07:46 +0530

    Create greater_small.c

---
+++ greater_small.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a,b ;
+   
+    printf("Enter a number: ");
+    scanf("%d %d", &a,&b);
+    
+    if (a>b) {
+       
+        printf("%d is greater than %d", a,b);
+    } 
+    else {
+       printf("%d is greater than %d", a,b);
+    }
+
+    return 0;
+}
