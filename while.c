8e5a4b7bc2e9c4cbd85eece3072f0a81524e55ab
Muskan Vaishnav <120307276+muskanv021@users.noreply.github.com>
2024-08-01 16:53:53 +0530

    Update while.c

--- while.c
+++ while.c
@@ -12,3 +12,40 @@ int main() {
      }
     return 0;
 }
+
+--------------------#################-----------------------------
+     
+#include <stdio.h>
+
+int main() {
+     int i;
+     i=10;
+     while(i>=0)
+     {
+     printf("\n Hello");
+     i--;
+         
+     }
+    return 0;
+}
+
+-----------------------###################33-----------------------
+     
+#include <stdio.h>
+
+int main() {
+     int i,s=0,n;
+     printf("Enter a number");
+     scanf("%d",&n);
+     
+     i=1;
+     while(i<=n)
+     {
+     s=s+i;
+     i++;
+     }
+     printf("\n sum of all number is:%d",s);
+    return 0;
+}
+-----------------------########################---------------------------------
+     
